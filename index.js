ba6cbbd5918c729ee5409a4d712a900b1729394c
Nanmaran Ramesh <e23it026@shanmugha.edu.in>
2025-01-29 17:45:24 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var sidenav = document.querySelector(".side-navbar")
+
+
+function showNav()
+{
+    sidenav.style.display = "block"
+}
+  
+function closeNav()
+{
+    sidenav.style.display = "none"
+}
